6d94604feccd149711b5fadead9901a0a22ff475
segmentationfaulter <muhammadsackib@gmail.com>
2019-11-07 07:44:22 +0500

    render input element

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,61 @@
-module Main exposing(..)
-import Html exposing (text)
+module Main exposing (..)
+
+import Browser
+import Html as H
+import Html.Attributes as Attr
 
 
 main =
-  text "Hello!"
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
+
+
+
+-- Model
+
+
+type alias Model =
+    { textInput : String
+    }
+
+
+initialModel : Model
+initialModel =
+    { textInput = ""
+    }
+
+
+
+-- Update
+
+
+type Msg
+    = InputChanged String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    model
+
+
+
+-- View
+
+
+view : Model -> H.Html Msg
+view model =
+    H.section
+        [ Attr.class "todoapp" ]
+        [ inputElement model ]
+
+
+inputElement : Model -> H.Html Msg
+inputElement model =
+    H.header
+        [ Attr.class "header" ]
+        [ H.h1 [] [ H.text "todos" ]
+        , H.input [ Attr.class "new-todo", Attr.placeholder "What needs to be done?", Attr.autofocus True ] []
+        ]
