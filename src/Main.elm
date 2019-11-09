081add1df0cdd721f64fbbdc3b36abbe88a31e34
segmentationfaulter <muhammadsackib@gmail.com>
2019-11-09 07:00:45 +0500

    add a new todo in model on pressing enter

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,7 @@ import Browser
 import Html as H
 import Html.Attributes as Attr
 import Html.Events as Events
+import Json.Decode as Decode
 
 
 main =
@@ -18,14 +19,22 @@ main =
 -- Model
 
 
+type alias Task =
+    { todo : String
+    , completed : Bool
+    }
+
+
 type alias Model =
     { newTodo : String
+    , tasks : List Task
     }
 
 
 initialModel : Model
 initialModel =
     { newTodo = ""
+    , tasks = []
     }
 
 
@@ -35,13 +44,17 @@ initialModel =
 
 type Msg
     = InputChanged String
+    | KeyPressed Int
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         InputChanged newTodo ->
-            Model newTodo
+            { model | newTodo = newTodo }
+
+        KeyPressed keyCode ->
+            createNewTodo keyCode model
 
 
 
@@ -60,5 +73,27 @@ inputElement model =
     H.header
         [ Attr.class "header" ]
         [ H.h1 [] [ H.text "todos" ]
-        , H.input [ Attr.class "new-todo", Attr.placeholder "What needs to be done?", Attr.autofocus True, Events.onInput InputChanged ] []
+        , H.input [ Attr.class "new-todo", Attr.placeholder "What needs to be done?", Attr.autofocus True, Events.onInput InputChanged, onKeyUp KeyPressed ] []
         ]
+
+
+
+-- Helpers
+
+
+onKeyUp : (Int -> msg) -> H.Attribute msg
+onKeyUp tagger =
+    Events.on "keyup" (Decode.map tagger Events.keyCode)
+
+
+createNewTodo : Int -> Model -> Model
+createNewTodo keyCode model =
+    let
+        enterKeyCode =
+            13
+    in
+    if keyCode == enterKeyCode then
+        Model "" (Task model.newTodo False :: model.tasks)
+
+    else
+        model
